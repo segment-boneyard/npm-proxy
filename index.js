cbfe728c2d3334e139aa327331f62eeaf664b620
TJ Holowaychuk <tj@vision-media.ca>
2013-12-03 18:00:11 -0800

    setup

---
+++ index.js
@@ -0,0 +1,23 @@
+
+/**
+ * Module dependencies.
+ */
+
+var assert = require('assert');
+
+/**
+ * Expose `Proxy`.
+ */
+
+module.exports = Proxy;
+
+/**
+ * Initialize a proxy.
+ *
+ * @param {Object} opts
+ * @api public
+ */
+
+function Proxy(opts) {
+  assert(opts, 'settings required');
+}
